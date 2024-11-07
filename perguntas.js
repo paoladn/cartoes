72427ac655c7aad3c26203f9f085b1af91ac7a0a
paola2 <166825551+paoladn@users.noreply.github.com>
2024-11-07 08:40:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+ criaCartao(
+        'Programação',
+        'O que é JavaScript?',
+        'O JavaScript é uma linguagem de programação'
+        )
+        criaCartao (
+        'Geografia',
+        'Qual a capital da França?',
+        'A capital da França é Paris'
+        )
+        criaCartao(
+        'Programação',
+        'O que é uma função?',
+        'Uma função é um bloco de código que executa alguma tarefa'
+        )
+        criaCartao(
+            'Língua Inglesa',
+            'Como se diz oi em Inglês?',
+            'Oi em inglês se diz HI (RAI)'
+            )
